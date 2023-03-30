66bd114f61a7373b918ebd0ab78fc3540232867d
Mohosin <mohosinali075@gmail.com>
2023-03-31 04:40:44 +0600

    create data.json file and also create fake json data

--- knowledge-cafe-project/src/components/Header/Header.jsx
+++ knowledge-cafe-project/src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <div className="dropdown dropdown-end">
           <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
             <div className="w-20 rounded-full border">
-              <img src="../../../public/img/user2.png" />
+              <img src="https://images.ctfassets.net/lh3zuq09vnm2/yBDals8aU8RWtb0xLnPkI/19b391bda8f43e16e64d40b55561e5cd/How_tracking_user_behavior_on_your_website_can_improve_customer_experience.png" />
             </div>
           </label>
         </div>
